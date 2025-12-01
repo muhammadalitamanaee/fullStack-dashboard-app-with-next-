d897205e2be81871c8aeb9f8592ac52e5b329775
muhammadAli tamanaee <alitaman200041@gmail.com>
2025-12-01 20:39:07 +0330

    add the search logic for invoices handled by params

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,8 +1,30 @@
-'use client';
+"use client";
 
-import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import {
+  useParams,
+  usePathname,
+  useRouter,
+  useSearchParams,
+} from "next/navigation";
+import { ChangeEvent } from "react";
 
 export default function Search({ placeholder }: { placeholder: string }) {
+  const serachParams = useSearchParams();
+  const { replace } = useRouter();
+  const pathname = usePathname();
+
+  const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    const val = event.target.value;
+    const params = new URLSearchParams(serachParams);
+    if (val) {
+      params.set("query", val);
+    } else {
+      params.delete("query");
+    }
+    replace(`${pathname}?${params.toString()}`);
+    console.log("Search input changed:", event.target.value);
+  };
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -11,6 +33,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
+        onChange={onChangeHandler}
+        defaultValue={serachParams.get("query")?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
