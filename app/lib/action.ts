45ecece66e65620a0dfccbf5b4d79078773bd9a4
muhammadAli tamanaee <alitaman200041@gmail.com>
2025-12-06 23:04:51 +0330

    create an invoice with server action and inseting in database

---
+++ app/lib/action.ts
@@ -0,0 +1,37 @@
+"use server";
+
+import { z } from "zod";
+import postgres from "postgres";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
+
+const formSchema = z
+  .object({
+    id: z.string(),
+    amount: z.coerce.number(),
+    customerId: z.string(),
+    status: z.enum(["pending", "paid"]),
+    date: z.string(),
+  })
+  .omit({ date: true, id: true });
+
+export async function createInvoice(formData: FormData) {
+  const { amount, customerId, status } = formSchema.parse({
+    customerId: formData.get("customerId"),
+    amount: formData.get("amount"),
+    status: formData.get("status"),
+  });
+  console.log("status in server", formData.get("status"));
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split("T")[0];
+
+  await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
+
+  revalidatePath("/dashboard/invoices");
+  redirect("/dashboard/invoices");
+}
