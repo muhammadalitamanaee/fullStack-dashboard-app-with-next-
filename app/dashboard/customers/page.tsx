41136fdba66a839d108a7f65bf6028cc6627f56c
muhammadAli tamanaee <alitaman200041@gmail.com>
2025-12-30 20:38:35 +0330

    adding meta data for pages

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,5 +1,8 @@
+import { Metadata } from "next";
 import React from "react";
-
+export const metadata: Metadata = {
+  title: "Customers",
+};
 export default function page() {
   return <div>customer page</div>;
 }
